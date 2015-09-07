c1e8346198c57895443a61422a70937b9cbe29d4
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-09-07 22:39:33 +1000

    Rendering blobber

--- js/blobber.js
+++ js/blobber.js
@@ -4,11 +4,24 @@ window.onload = function(){
     x: 10,
     y: 10,
     image: new Image(),
+    speed: 3,
   },
   c = document.getElementById("blobber"),
   ctx = c.getContext("2d");
 
   player.image.src = "images/blobber.png";
+
+  window.onkeydown = function(e){
+    if (e.keyCode == 37) {
+      player.x -= player.speed;
+    } else if (e.keyCode == 38) {
+      player.y -= player.speed;
+    } else if (e.keyCode == 39) {
+      player.x += player.speed;
+    } else if (e.keyCode == 40) {
+      player.y += player.speed;
+    }
+  };
 
   (function render(){
     window.requestAnimationFrame(render);
